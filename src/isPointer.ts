a93b05fe9ce4f8b166044be77d3a1342398a0f40
Jason Wijegooneratne <admin@jwije.com>
2017-09-12 18:19:18 +0700

    update docs

--- src/isPointer.ts
+++ src/isPointer.ts
@@ -1,7 +1,25 @@
+// The `isPointer` function determines if its argument is a valid json pointer
+// or not returning a boolean indicating the result of the operation.
+
+// ## Usage
+
+// ```javascript
+// import { isPointer } from '@jdw/jst';
+//
+// isPointer('#/foo');   // true
+// isPointer({})         // false
+// ```
+
+// ## Dependencies
+
 import { IsPointer } from './types';
 
-// A JSON `pointer` must begin with the symbols '#', '/' or be an empty string ''.
+// ## Implementation
+
 export const isPointer: IsPointer = (input) => {
+  // A JSON `pointer` must begin with the symbols '#', '/' or be an empty string
+  // ''.
+
   if (typeof input !== 'string') {
     return false;
   }
@@ -13,6 +31,6 @@ export const isPointer: IsPointer = (input) => {
   if (/^#|^\//.test(input)) {
     return true;
   }
-
+  // If it is not one of these values then `input` is not a json pointer.
   return false;
 };
